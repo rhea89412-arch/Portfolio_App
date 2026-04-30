0fe5f7004af3c1aab3e57b2237dc42ac11c5e422
rhea89412-arch <rhea89412@gmail.com>
2026-04-30 10:51:16 +0800

    Update 3_😊_About.py

--- CORTEZ_BSCS-3C_ACTIVITY2/pages/3_😊_About.py
+++ CORTEZ_BSCS-3C_ACTIVITY2/pages/3_😊_About.py
@@ -40,7 +40,7 @@ st.markdown("---")
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    st.image("pages\Cortez_Picture2.jpg", caption="My Profile")
+    st.image("CORTEZ_BSCS-3C_ACTIVITY2/pages/Cortez_Picture2.jpg", caption="My Profile")
 
 with col2:
     st.markdown('<div class="card">', unsafe_allow_html=True)
